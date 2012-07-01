c6baaaa276ab50782f17caa27673c2d00624725d
Christopher Wright <christopherwright@gmail.com>
2012-07-01 16:22:06 +1000

    Boilerplate

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'eventmachine', '~> 0.12.10'
+gem 'rspec', '~> 2.10.0'
---
+++ spec/bluecap_spec.rb
@@ -0,0 +1,6 @@
+require 'bluecap'
+
+describe 'Bluecap' do
+  it "can create an identifier for a user" do
+  end
+end
