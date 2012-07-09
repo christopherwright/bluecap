bb4708416f8b2f3c17d8bbe4279d6456009d812a
Christopher Wright <christopherwright@gmail.com>
2012-07-10 00:55:23 +1000

    Construct events bitfield name from key and timestamp

--- lib/bluecap/handlers/event.rb
+++ lib/bluecap/handlers/event.rb
@@ -1,7 +1,24 @@
+require 'date'
+
 module Bluecap
   class Event
+    def clean_name(name)
+      name.strip.downcase.gsub(/[^a-z0-9]/, '.')
+    end
+
+    def date(timestamp)
+      Time.at(timestamp).strftime('%Y%m%d')
+    end
+
+    def key(name, timestamp=Time.now.to_i)
+      return "events:#{clean_name(name)}:#{date(timestamp)}"
+    end
+
     def handle(data)
-      puts data
+      Bluecap::Redis.setbit(
+        key(data[:event][:name], data[:event][:timestamp]),
+        data[:event][:id],
+        1)
     end
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,6 +4,28 @@ describe 'Event handler' do
 
   before do
     Bluecap.redis.flushall
+    @event = Bluecap::Event.new
+  end
+
+  it 'should strip surrounding whitespace from event name' do
+    @event.clean_name('  paid  ').should eq('paid')
+  end
+
+  it 'should not allow capital letters in event name' do
+    @event.clean_name('SignUp').should eq('signup')
+  end
+
+  it "should convert non-alphanumeric characters that aren't
+  leading/trailing to periods" do
+    @event.clean_name('  logged in  ').should eq('logged.in')
+  end
+
+  it "should convert unix timestamps to strings in %Y%m%d format" do
+    @event.date(1341845456).should eq('20120710')
+  end
+
+  it "should create event key using cleaned name and date" do
+    @event.key('  Sign Up', 1341845456).should eq ('events:sign.up:20120710')
   end
 
 end
