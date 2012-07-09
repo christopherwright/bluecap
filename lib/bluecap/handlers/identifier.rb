8ed21cb8aaa4da1de417ef2a10520d619f92d7ee
Christopher Wright <christopherwright@gmail.com>
2012-07-09 23:18:52 +1000

    Identifier.handle always returns a fixnum

--- lib/bluecap/handlers/identifier.rb
+++ lib/bluecap/handlers/identifier.rb
@@ -4,16 +4,16 @@ module Bluecap
     def handle(data)
       name = data[:identifier]
       id = Bluecap.redis.hget('user.map', name)
-      if id.nil?
-        id = Bluecap.redis.incr('user.ids')
-        if Bluecap.redis.hsetnx('user.map', name, id)
-          return id
-        else
-          # Race condition, another process has set the id for this user.
-          # The unused id shouldn't cause inaccuracies in reporting since
-          # the position will be 0 in all bitsets across the system.
-          return redis.hget('user.map', name)
-        end
+      return id.to_i if id
+
+      id = Bluecap.redis.incr('user.ids')
+      if Bluecap.redis.hsetnx('user.map', name, id)
+        return id
+      else
+        # Race condition, another process has set the id for this user.
+        # The unused id shouldn't cause inaccuracies in reporting since
+        # the position will be 0 in all bitsets across the system.
+        return redis.hget('user.map', name).to_i
       end
     end
 
