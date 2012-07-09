3098f710df07a88beb9a4cd4dc2774b684cfe298
Christopher Wright <christopherwright@gmail.com>
2012-07-09 23:09:31 +1000

    Make comment more explicit, less confusing

--- lib/bluecap/handlers/identifier.rb
+++ lib/bluecap/handlers/identifier.rb
@@ -11,7 +11,7 @@ module Bluecap
         else
           # Race condition, another process has set the id for this user.
           # The unused id shouldn't cause inaccuracies in reporting since
-          # it will be 0 in all bitfields across the system.
+          # the position will be 0 in all bitsets across the system.
           return redis.hget('user.map', name)
         end
       end
