e9e843db5f8c025375c453df8f222c8b8b2d6fdc
Christopher Wright <christopherwright@gmail.com>
2012-07-10 21:32:44 +1000

    Change double quotes to single

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -20,11 +20,11 @@ describe 'Event handler' do
     @event.clean_name('  logged in  ').should eq('logged.in')
   end
 
-  it "should convert unix timestamps to strings in %Y%m%d format" do
+  it 'should convert unix timestamps to strings in %Y%m%d format' do
     @event.date(1341845456).should eq('20120710')
   end
 
-  it "should create event key using cleaned name and date" do
+  it 'should create event key using cleaned name and date' do
     @event.key('Sign Up', 1341845456).should eq ('events:sign.up:20120710')
   end
 
