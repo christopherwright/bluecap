f6bd3804e70ec15ae50aa953bb698845c878dfa0
Christopher Wright <christopherwright@gmail.com>
2012-07-12 00:30:31 +1000

    More report format, work in progress

--- lib/bluecap/handlers/report.rb
+++ lib/bluecap/handlers/report.rb
@@ -1,26 +1,37 @@
 module Bluecap
   class Report
+    # Generates a cohort report for the client. Finds the time between when
+    # a person has tracked two events (e.g.: created an account then logged
+    # back in).
+    #
+    # data - A Hash containing report options.
+    #        :events     -
+    #        :dates      -
+    #        :attributes -
+    #        :buckets    -
+    #        :across     -
+    #
+    # Examples
+    #
+    #   handle({
+    #     events: {
+    #       from: 'Created Account',
+    #       to: 'Logged In'
+    #     },
+    #     dates: {
+    #       from: '2012-03-17',
+    #       to: '2012-06-17
+    #     },
+    #     attributes: {
+    #       country: 'Australia',
+    #       gender: 'Female'
+    #     },
+    #     buckets: 'weekly',
+    #     across: 'weekly'
+    #   })
+    #
+    # Returns the String with report data in JSON format.
     def handle(data)
-      """
-      data = {
-        events: {
-          from: 'Created Account',
-          to: 'Logged In'
-        },
-        dates: {
-          from: '2012-03-17',
-          to: '2012-06-17'
-        },
-        attributes: {
-          country: 'Australia',
-          gender: 'Male',
-        },
-        buckets: 'weekly',
-        across: 'weekly'
-      }
-      """
-
-      1
     end
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -10,24 +10,26 @@ describe 'Report handler' do
   it 'should generate report' do
     # TODO: Split this into several tests once design takes shape.
     data = {
-      events: {
-        from: 'Created Account',
-        to: 'Logged In'
-      },
-      dates: {
-        from: '2012-03-17',
-        to: '2012-06-17'
-      },
-      attributes: {
-        country: 'Australia',
-        gender: 'Male',
-      },
-      buckets: 'weekly',
-      across: 'weekly'
+      report: {
+        events: {
+          from: 'Created Account',
+          to: 'Logged In'
+        },
+        dates: {
+          from: '2012-03-17',
+          to: '2012-06-17'
+        },
+        attributes: {
+          country: 'Australia',
+          gender: 'Male',
+        },
+        buckets: 'weekly',
+        across: 'weekly'
+      }
     }
 
     results = @report.handle(data)
-    results.should eql(1)
+    results.should eql(nil)
   end
 
 end
