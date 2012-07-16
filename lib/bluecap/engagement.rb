6fe45ada06499e257e76a964b1d8611f42939f09
Christopher Wright <christopherwright@gmail.com>
2012-07-16 14:36:01 +1000

    Add documentation for engagement

--- lib/bluecap/engagement.rb
+++ lib/bluecap/engagement.rb
@@ -1,6 +1,25 @@
 module Bluecap
   class Engagement
 
+    # Initialize an Engagement class to measure Cohort retention.
+    #
+    # options - A hash containing options that define the engagement.
+    #           :cohort           - The Cohort to measure engagement for.
+    #           :engagement_event - The String event that defines engagement.
+    #           :start_date       - The Date starting period to measure
+    #                               engagement for.
+    #           :end_date         - The Date ending period to measure
+    #                               engagement for.
+    #           :report_id        - The Integer identifier of the report this
+    #                               cohort is being used with.
+    #
+    # Examples
+    #
+    #   engagement = Bluecap::Engagement.new :cohort => cohort,
+    #     :engagement_event => 'Logged In',
+    #     :start_date => Date.parse('20120702'),
+    #     :end_date => Date.parse('20120707'),
+    #     :report_id => 1
     def initialize(options)
       @cohort = options.fetch(:cohort)
       @engagement_event = options.fetch(:engagement_event)
@@ -9,14 +28,29 @@ module Bluecap
       @report_id = options.fetch(:report_id)
     end
 
+    # A key name in Redis where the bitcount operation for engagement on a
+    # date should be stored.
+    #
+    # Returns the String key name.
     def key(date)
       Bluecap::Keys.engagement(@report_id, @cohort.id, date.strftime('%Y%m%d'))
     end
 
+    # The keys that comprise the bitmask for the engagement on a date.
+    #
+    # Returns the Array of String keys.
     def keys(date)
       [@cohort.key, Bluecap::Keys.event(@engagement_event, date.strftime('%Y%m%d'))]
     end
 
+    # Measure the engagement of a cohort over a given period.
+    #
+    # Examples
+    #
+    #    measure
+    #    # => {"20120702" =>  100.0, "20120703" => 50.0, ...}
+    #
+    # Returns a Hash with engagement percentages for each day in the period.
     def measure
       results = Hash.new
       (@start_date..@end_date).each do |date|
