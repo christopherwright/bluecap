4259be93971e09806a8d170a89ce2737f88954b0
Christopher Wright <christopherwright@gmail.com>
2012-07-17 02:11:26 +1000

    Log messages routed to NullHandler

--- lib/bluecap/handlers/null_handler.rb
+++ lib/bluecap/handlers/null_handler.rb
@@ -1,14 +1,18 @@
 module Bluecap
   class NullHandler
 
+    attr_reader :data
+
     # Initialize a NullHandler.
-    def initialize(*args)
+    def initialize(data)
+      @data = data
     end
 
     # Catch the handle message.
     #
     # Returns nothing.
     def handle
+      Bluecap.log "NullHandler received message, contents: #{@data}"
     end
 
   end
